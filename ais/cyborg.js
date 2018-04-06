3c066ccefbfb1e6c60a1fe266a6ff4745c8215bc
Rio6 <rio6.liu@gmail.com>
2018-04-05 18:08:31 -0700

    My cyborg ai

---
+++ ais/cyborg.js
@@ -0,0 +1,65 @@
+r26Ai.clearAiRule();
+
+r26Ai.addAiRule({
+    filter: unit => unit.name === "BERRY",
+    ai: function(unit) {
+        this.run = function() {
+
+            var enemy = order.findThings(tgt =>
+                tgt.unit && tgt.side === otherSide(unit.side), 1000)[0];
+            if(enemy) {
+                order.follow(enemy);
+                if(v2.distance(unit.pos, enemy.pos) < 1200)
+                    return;
+            }
+
+            var avoidDest = movement.avoidShots(20, bullet => !bullet.instant);
+            if(avoidDest) {
+                order.move(avoidDest);
+                return;
+            }
+
+            if(enemy) return;
+
+            var point = movement.spread(order.findThings(target =>
+                target.commandPoint &&
+                (target.side !== unit.side || target.capping > 0)));
+            if(point) {
+                var tgtPos = movement.inRange(point.pos, point.radius);
+                if(tgtPos) {
+                    order.move(tgtPos);
+                    return;
+                }
+            }
+        }
+    }
+});
+
+r26Ai.addAiRule({
+    filter: unit => JSON.stringify(unit.spec) === "{\"parts\":[{\"pos\":[-30,0],\"type\":\"Engine04\",\"dir\":0},{\"pos\":[10,30],\"type\":\"Solar1x1\",\"dir\":0},{\"pos\":[-10,30],\"type\":\"Wing1x1Notch\",\"dir\":0},{\"pos\":[30,10],\"type\":\"Solar1x1\",\"dir\":0},{\"pos\":[-30,30],\"type\":\"Wing1x1Notch\",\"dir\":0},{\"pos\":[30,-10],\"type\":\"Solar1x1\",\"dir\":0},{\"pos\":[30,30],\"type\":\"Battery1x1\",\"dir\":0},{\"pos\":[0,0],\"type\":\"CloakGenerator\",\"dir\":0}],\"name\":\"\",\"aiRules\":[[\"Finish player orders\"],[\"@attackTypes enemy that is @absoluteTypes then # within #m\",\"Flee\",\"more DPS\",1,900],[\"When #% of energy, @chargeTypes\",5,\"flee enemies\"],[\"@capTypes command points within #m\",\"Spread to\",10000]]}",
+    ai: function(unit) {
+        this.run = function() {
+
+            var enemy = order.findThings(1000, tgt =>
+                tgt.unit && tgt.side === otherSide(unit.side))[0];
+            if(enemy) {
+                order.move(movement.fleeRange(enemy.pos, enemy.weaponRange));
+                return;
+            }
+
+            var point = movement.spread(order.findThings(target =>
+                target.commandPoint &&
+                (target.side !== unit.side || target.capping > 0)));
+            if(point) {
+                var tgtPos = movement.inRange(point.pos, point.radius);
+                if(tgtPos) {
+                    order.move(tgtPos);
+                    return;
+                }
+            }
+        }
+    }
+});
+
+r26Ai.enabled = true;
+
